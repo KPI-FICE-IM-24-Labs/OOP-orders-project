4a0b50977473e6a31ac0014d19d5fd6bcbef8208
Kiril Yakymchuk <stbestich@gmail.com>
2025-05-08 14:55:36 +0300

    chore(order-api): added some configurations
    * Config module
    * Logger module
    * Validation pipe

--- order-api/src/app.module.ts
+++ order-api/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        HOST: Joi.string().hostname().optional().default('localhost'),
+        PORT: Joi.number().port().required(),
+      }),
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- order-api/src/main.ts
+++ order-api/src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import { Logger as PinoLogger } from 'nestjs-pino';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    cors: true,
+  });
+
+  const config = app.get<ConfigService>(ConfigService);
+  const logger = new Logger(bootstrap.name);
+
+  const HOST = config.getOrThrow<string>('HOST');
+  const PORT = config.getOrThrow<number>('PORT');
+
+  app.useLogger(app.get<PinoLogger>(PinoLogger));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  app.enableCors();
+  app.use(helmet());
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  await app.listen(PORT, HOST, () => {
+    logger.log(`Service is running on http://${HOST}:${PORT}`);
+  });
 }
 void bootstrap();
